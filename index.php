57bec52bc9bb4ed956529107701ec0a4f6c5232f
b33c00l <psakalauskas@gmail.com>
2017-11-02 12:45:48 +0200

    Composer and Whoops install

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <pre>
 <?php
 
-spl_autoload_register(function ($class_name){
-	include "Classes/".$class_name. '.php';
-});
+require 'vendor/autoload.php';
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+$whoops->register();
+
+// spl_autoload_register(function ($class_name){
+// 	include "Classes/".$class_name .'.php';
+// });
 
 // require "Classes/TransportInterface.php";
 // require "Classes/Car.php";
 // require "Classes/ElectricCar.php";
 
 
-$petro_automobilis = new Car(4, 4, 1500, 1900);
-$jono_automobilis = new ElectricCar(3, 1, 300, 550);
+// $petro_automobilis = new Car(4, 4, 1500, 1900);
+// $jono_automobilis = new ElectricCar(3, 1, 300, 550);
 
 // //by default
 // echo $petro_automobilis->doorCount;
-
 // //changing parameter
 // $petro_automobilis->doorCount = 8;
 
@@ -36,5 +41,5 @@ $jono_automobilis = new ElectricCar(3, 1, 300, 550);
 
 // echo $petro_automobilis;
 
-$petro_automobilis->go();
-$jono_automobilis->go();
+// $petro_automobilis->go();
+// $jono_automobilis->go();
